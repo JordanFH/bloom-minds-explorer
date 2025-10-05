8f83a7ce9e3c4eed0565eaa924fb561b6c74db72
PBurgos27 <pablo.burgosr27@gmail.com>
2025-10-05 01:57:31 -0500

    fix(footer): fixed Image width and heigh

--- src/components/footers/landing-footer.tsx
+++ src/components/footers/landing-footer.tsx
@@ -6,7 +6,13 @@ export default function LandingFooter() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-3">
-            <Image src="/logo.png" alt="Bloom Minds Logo" className="w-6 h-6" />
+            <Image
+              src="/logo.png"
+              alt="Bloom Minds Logo"
+              className="w-6 h-6"
+              width={100}
+              height={100}
+            />
             <span className="font-bold">
               Bloom <span className="text-primary">Minds</span> Explorer
             </span>
